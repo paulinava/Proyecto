a7f627b2162c1afd4798c3fed9aeb1f2d28371a9
paulinava <47132525+paulinava@users.noreply.github.com>
2020-08-28 23:15:15 -0500

    Add files via upload

---
+++ avance.py
@@ -0,0 +1,30 @@
+#titulo            :control de entradas
+#description       :dejar pasa
+#autor             :Paulina Viveros  
+#python_version    :3.5.2
+#=======================================================
+#para registrar el numero de entradas
+#funcion regresa True o False
+def entrada (val):
+    if (val > 1):
+        return True
+    else:
+        return False
+#para que el caso sea correto, las salidas deberan ser iguales
+entradas = 0
+if entrada(4) == True:
+    entradas = entradas + 1
+    print("es Xr debe ser Xr" (True))
+if entrada(0) == False:
+    entradas = entradas 
+    print(" es Xr debe ser Xr" (False))
+if entrada(2) == True:
+    entradas = entradas + 1
+    print("es Xr debe ser Xr" (True))
+    
+    
+
+
+
+
+
