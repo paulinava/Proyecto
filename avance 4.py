d817eaca1cbea9ba32118862e99467d004dd16f5
paulinava <47132525+paulinava@users.noreply.github.com>
2020-09-25 21:38:40 -0500

    Add files via upload

---
+++ avance 4.py
@@ -0,0 +1,18 @@
+
+print "Deslizar tarjeta"
+print "Ingresar número de empleado"
+
+i = 0
+Salida = True
+while Salida:
+    i = i +1
+    print ("Adelante")
+    if i > 100:
+        salida = False
+        print ("Salida denegada")
+    else i <= 100:
+        salida = True
+        print ("Adelante")
+
+    
+
